6a7009ac7d24f54a9993578f532c84cab845baf3
NIKHIL PRAJAPATI <nikboy010@gmail.com>
2024-10-26 15:15:01 +0530

    fix issue

--- weebserver/settings.py
+++ weebserver/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ky)9v56)np$*12t3p_j&q$31z&tlf(*p2x9empe&=w!bqd8zrh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
