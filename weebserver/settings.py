9b1ff4f6fcfba09a4cc1f2fb802113991ab0de53
NIKHIL PRAJAPATI <nikboy010@gmail.com>
2024-12-03 20:51:20 +0530

    fix document

--- weebserver/settings.py
+++ weebserver/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-ky)9v56)np$*12t3p_j&q$31z&tlf(*p2x9empe&=w!bqd8zrh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','*']
+ALLOWED_HOSTS = ['.vercel.app','*',"127.0.0.1", "localhost", "salah99.pythonanywhere.com"]
+
+
 
 # Application definition
 
